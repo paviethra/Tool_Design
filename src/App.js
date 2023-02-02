34dedc12f3c207d7ff70e3db74a6a2532f61c02c
paviethra <62457057+paviethra@users.noreply.github.com>
2023-02-02 09:20:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,10 +28,11 @@ function App() {
 
       </section>
 
-    {/* Card holder */}
-      <section className='card-holder'>
-        <div className='card-heading'>
-          <i className='fa fa-comments'></i>
+  <div className='data-wrapper'>
+      {/* Card holder (or)Test Suites */}
+      <section className='left'>
+        <div className='heading'>
+          <i className='fa fa-flag'></i>
           <h5>Test Suits</h5>
         </div>
         <hr />
@@ -46,7 +47,7 @@ function App() {
               <section className='card-tp-left'>
                 <img
                   src='https://media.istockphoto.com/id/1198530470/vector/vector-illustration-of-squirrel-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=Mvp9X_ywmSfD4JYQdeIvqKWuNPuoYRKkYF4eJdmN6JU='
-                  style={{ width: '5em', height: '5em' }}
+                  style={{ width: '3.5em', height: '3.5em' }}
                   alt='No_Image' />
                 <progress max={100} value={85}></progress>
               </section>
@@ -87,7 +88,7 @@ function App() {
               <section className='card-tp-left'>
                 <img
                   src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUqvJwOdjnhMZF2IpmhHbyZ7tlc0yux2oZ3A&usqp=CAU'
-                  style={{ width: '5em', height: '5em' }}
+                  style={{ width: '3.5em', height: '3.5em' }}
                   alt='No_Image' />
                 <progress max={100} value={85}></progress>
               </section>
@@ -127,7 +128,7 @@ function App() {
               <section className='card-tp-left'>
                 <img
                   src='https://t4.ftcdn.net/jpg/03/22/27/15/360_F_322271599_m6B4XfH1Y2k72n5Kk34Q1KfX7oz3GlQs.jpg'
-                  style={{ width: '5em', height: '5em' }}
+                  style={{ width: '3.5em', height: '3.5em' }}
                   alt='No_Image' />
                 <progress max={100} value={85}></progress>
               </section>
@@ -164,6 +165,92 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* Empty Section */}
+      <section className='empty'></section>
+
+      {/* Whats new area */}
+      <section className='right'>
+            <div className='whats-new'>
+              <div className='heading'>
+                  <i className='fa fa-gift'></i>
+                  <h5>What's New</h5>
+              </div>
+              <hr/>
+              <input type='text' placeholder='No new annoncements'/>
+            </div>
+      {/* Statistics area */}
+            <div className='statistics'>
+            <div className='heading'>
+                  <i className='fa fa-bar-chart'></i>
+                  <h5>Statistics</h5>
+              </div>
+              <hr/>
+                <div className='user small-box box'>
+                  <div className='small-box'>
+                    <i className='fa fa-user'></i>
+                    <h6>Users</h6>
+                  </div>
+                  <p>3</p>
+                </div>
+
+
+                <div className='suites small-box box'>
+                  <div className='small-box'>
+                    <i className='fa fa-flag'></i>
+                    <h6>Test Suites</h6>
+                  </div>
+                  <p>247</p>
+                </div>
+
+                <div className='defects small-box box'>
+                  <div className='small-box'>
+                    <i className='fa fa-flag'></i>
+                    <h6>Test Suites</h6>
+                  </div>
+                  <p>383</p>
+                </div>
+
+                <div className='runs small-box box'>
+                  <div className='small-box'>
+                      <i className='fa fa-gear'></i>
+                      <h6>Runs</h6>
+                  </div>
+                  <p>436368</p>
+                </div>
+
+                <div className='testbeds small-box box'>
+                  <div className='small-box'>
+                    <i className='fa fa-pencil'></i>
+                    <h6>Testbeds</h6>
+                  </div>
+                  <p>1000</p>
+                </div>
+
+            </div>
+
+      {/* Event area */}
+      <div className='events'>
+          <div className='heading'>
+              <i className='fa fa-calendar'></i>
+              <h5>Events</h5>
+          </div>
+          <hr/>
+            <div className='small-box  box'>
+              <h6>Rajalingam Govindaraj (c) runs</h6>
+              <i className='fa fa-envelope'></i>
+            </div>
+            <div className='small-box box'>
+              <h6>Abdul Subedar (c) runs</h6>
+              <i className='fa fa-envelope'></i>
+            </div>
+            <div className='small-box box'>
+                <h6>Abdul Subedar (c) runs</h6>
+                <i className='fa fa-envelope'></i>
+            </div>
+      </div>
+      </section>
+  </div>
     </>
   );
 }
